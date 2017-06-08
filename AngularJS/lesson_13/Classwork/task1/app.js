3f9bbdc8d69a27f3b9b4de0bec996ff0594756cc
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-06-09 01:02:11 +0300

    added class work 1 for lesson 3

--- AngularJS/lesson_13/Classwork/task1/app.js
+++ AngularJS/lesson_13/Classwork/task1/app.js
@@ -1,4 +1,14 @@
 var app = angular.module("myApp", []);
 app.controller("mainCtrl", function ($scope) {
+    $scope.classNames = "test-div lightGreen";
+    $scope.changeBg = function () {
+
+        if ($scope.classNames = "test-div lightGreen"){
+            $scope.classNames = "test-div lightBlue";
+        }else{
+            $scope.classNames = "test-div lightGreen";
+        }
+
+    }
 
 });
