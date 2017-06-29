8fb9f850ee4358e48fe5ab6a5ccea741c6c3c72c
Yurik <pyurik44@gmail.com>
2017-06-29 16:30:36 +0300

    delete

--- React/lesson_19/ClassWork/task1/app.jsx
+++
@@ -1,4 +0,0 @@
-var elem = React.createElement('h1', null, 'Hello World!');
-        // Функция render принимает два аргумента: виртуальный элемент и реальный узел DOM.
-        // React берёт виртуальный элемент и добавляет его в указанный узел.
-        ReactDOM.render(elem, document.getElementById('example'));
