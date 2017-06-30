de4d81675d7dd341a20ee078e6869c23ca724a7b
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-07-01 01:03:48 +0300

    Добавленны Task

--- React/lesson_20/webpack.config.js
+++ React/lesson_20/webpack.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     entry: {
-        "ClassWork/task2/build/bundle": "./ClassWork/task2/jsx/main.jsx",
-        "ClassWork/task3/build/bundle": "./ClassWork/task3/jsx/main.jsx",
-        "HomeWork/task1/build/bundle": "./HomeWork/task1/jsx/main.jsx",
-        "HomeWork/task2/build/bundle": "./HomeWork/task2/jsx/main.jsx",
-        "HomeWork/task3/build/bundle": "./HomeWork/task3/jsx/main.jsx"
+        "ClassWork/task1/build/bundle": "./ClassWork/task1/jsx/main.jsx"
     },
 
     output:{
