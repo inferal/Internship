e2a5cfffae3f79cfe52f9bf4bdb4d2ab924d078a
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-07-01 01:00:13 +0300

    Настройка

---
+++ React/lesson_20/webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: {
+        "ClassWork/task2/build/bundle": "./ClassWork/task2/jsx/main.jsx",
+        "ClassWork/task3/build/bundle": "./ClassWork/task3/jsx/main.jsx",
+        "HomeWork/task1/build/bundle": "./HomeWork/task1/jsx/main.jsx",
+        "HomeWork/task2/build/bundle": "./HomeWork/task2/jsx/main.jsx",
+        "HomeWork/task3/build/bundle": "./HomeWork/task3/jsx/main.jsx"
+    },
+
+    output:{
+        filename: '[name].js'
+    },
+    module:{
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                query:
+                    {
+                        presets: ['react']
+                    }
+            }
+        ]
+    }
+};
