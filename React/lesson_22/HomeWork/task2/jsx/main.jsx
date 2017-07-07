ea9a3f485d0903a805b234696086de9d5e6f86fe
Yurik <pyurik44@gmail.com>
2017-07-07 16:43:28 +0300

    added HomeWork Task2 for lesson_22

---
+++ React/lesson_22/HomeWork/task2/jsx/main.jsx
@@ -0,0 +1,53 @@
+/*
+ * Home Work
+ *
+ * ### Задача 2
+ *
+ * Создайте класс User, который наследует от класса Person (из предыдущей задачи),
+ * со свойствами signUpDate(дата регистрации, по умолчанию 0) и id.
+ * Создайте несколько экземпляров класса и запишите их в массив users.
+ */
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class Person{
+    constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male"){
+            this.firstName = firstName,
+            this.lastName = lastName,
+            this.age = age,
+            this.gender = gender
+    }
+
+    fullName(){
+        let fuName = this.firstName + ' ' + this.lastName;
+        return fuName;
+    }
+
+    sayHi(){
+        let saHi = "Hello, my name is " + this.fullName();
+        return saHi;
+    }
+}
+
+class User extends Person{
+    constructor(firstName, lastName, age, gender, signUpDate, id){
+        super(firstName, lastName, age, gender);
+        this.signUpDate = signUpDate;
+        this.id = id;
+    }
+}
+
+const users = [
+    user= new User('Yurik', 'Pop', 15, 'Male', Date.now(), 1),
+    user= new User('Vas', 'Poli', 20,'Male', Date.now(), 2),
+    user= new User('Vik', 'Rok', 35, 'Male', Date.now(), 3)
+
+];
+
+const person = new Person();
+console.log(person.sayHi());
+
+console.log(users);
