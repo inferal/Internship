e893c41b7a92255dfd3aecb5a6161b4ef2414a10
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-07-10 22:07:54 +0300

    webpack.config.js for lesson_22

--- React/lesson_22/webpack.config.js
+++ React/lesson_22/webpack.config.js
@@ -1,11 +1,11 @@
 module.exports = {
     entry: {
-        "ClassWork/task1/build/bundle": "./ClassWork/task1/jsx/main.jsx",
-        "ClassWork/task2/build/bundle": "./ClassWork/task2/jsx/main.jsx",
-        "ClassWork/task3/build/bundle": "./ClassWork/task3/jsx/main.jsx",
-        "HomeWork/task1/build/bundle": "./HomeWork/task1/jsx/main.jsx",
-        "HomeWork/task2/build/bundle": "./HomeWork/task2/jsx/main.jsx",
-        "HomeWork/task3/build/bundle": "./HomeWork/task3/jsx/main.jsx"
+        "ClassWork/task1/build/bundle": "./ClassWork/task1/jsx/main.js",
+        "ClassWork/task2/build/bundle": "./ClassWork/task2/jsx/main.js",
+        "ClassWork/task3/build/bundle": "./ClassWork/task3/jsx/main.js",
+        "HomeWork/task1/build/bundle": "./HomeWork/task1/jsx/main.js",
+        "HomeWork/task2/build/bundle": "./HomeWork/task2/jsx/main.js",
+        "HomeWork/task3/build/bundle": "./HomeWork/task3/jsx/main.js"
     },
 
     output:{
