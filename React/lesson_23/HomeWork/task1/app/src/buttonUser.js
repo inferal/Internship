5c4f77b48d708d9f3b8f7cb3290941108120dd1b
DESKTOP-RHPFIMF\yurik <pyurik44@gmail.com>
2017-07-10 23:07:04 +0300

    added Home work Task1 for lesson_23

---
+++ React/lesson_23/HomeWork/task1/app/src/buttonUser.js
@@ -0,0 +1,11 @@
+import React, { Component }from 'react';
+
+class ButtonUser extends Component{
+    render(){
+        return(
+            <input type="button" value="New Users"/>
+        )
+    }
+}
+
+export default ButtonUser;
