ad18a2208be572420fee23a2556db062a5ec4cd9
Yurik <pyurik44@gmail.com>
2017-07-11 16:48:36 +0300

    start home work lesson_24

---
+++ React/lesson_24/HomeWork/task1/app/views/listView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class View1 extends Component{
+
+    render(){
+        return(
+            <h1>Это текст View1</h1>
+        )
+    }
+}
+
+export default View1;
